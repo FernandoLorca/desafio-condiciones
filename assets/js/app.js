5389a3a77df25df918bf5872c5a79d79a54b060c
FernandoLorca <florcapinto@gmail.com>
2022-10-27 20:02:14 -0300

    functionaliry first excersice done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const imgButton = document.querySelector(".excersiceOne__img");
+console.log(imgButton.classList.contains("borderRedActive"));
+
+imgButton.addEventListener("click", () => {
+  if (!imgButton.classList.contains("borderRedActive")) {
+    imgButton.classList.add("borderRedActive");
+  } else {
+    imgButton.classList.remove("borderRedActive");
+  }
+});
