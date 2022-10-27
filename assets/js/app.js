21b5692b8210749e4c2f2221941038a808a5d54b
FernandoLorca <florcapinto@gmail.com>
2022-10-27 20:02:45 -0300

    functionaliry first excersice done

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@
 const imgButton = document.querySelector(".excersiceOne__img");
 console.log(imgButton.classList.contains("borderRedActive"));
 
+//excersiceOne
 imgButton.addEventListener("click", () => {
   if (!imgButton.classList.contains("borderRedActive")) {
     imgButton.classList.add("borderRedActive");
