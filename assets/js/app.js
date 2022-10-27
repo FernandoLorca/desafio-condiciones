eb4e8b9519c668fd64d2391611b4636983ec5a84
FernandoLorca <florcapinto@gmail.com>
2022-10-27 20:34:17 -0300

    html and css of second sxcercise is done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,9 @@
 "use strict";
 
+//excersiceOne
 const imgButton = document.querySelector(".excersiceOne__img");
 console.log(imgButton.classList.contains("borderRedActive"));
 
-//excersiceOne
 imgButton.addEventListener("click", () => {
   if (!imgButton.classList.contains("borderRedActive")) {
     imgButton.classList.add("borderRedActive");
