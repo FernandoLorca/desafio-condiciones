5d5986b594cef18838a632e2a7fa3dab6c3408ba
FernandoLorca <florcapinto@gmail.com>
2022-10-27 21:18:23 -0300

    functionality of second excersice is done

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,39 @@
 
 //excersiceOne
 const imgButton = document.querySelector(".excersiceOne__img");
-console.log(imgButton.classList.contains("borderRedActive"));
 
 imgButton.addEventListener("click", () => {
   if (!imgButton.classList.contains("borderRedActive")) {
     imgButton.classList.add("borderRedActive");
   } else {
     imgButton.classList.remove("borderRedActive");
+  }
+});
+
+//excersiceTwo
+const stickerInputOne = document.querySelector("#stickerInput1");
+const stickerInputTwo = document.querySelector("#stickerInput2");
+const stickerInputThree = document.querySelector("#stickerInput3");
+const totalNumberSticker = document.querySelector("#totalNumberSticker");
+const stickerWarning = document.querySelector(".stickerWarning");
+const buttonStickers = document.querySelector(".buttonStickers");
+
+buttonStickers.addEventListener("click", () => {
+  const inputOne = Number(stickerInputOne.value);
+  const inputTwo = Number(stickerInputTwo.value);
+  const inputThree = Number(stickerInputThree.value);
+  const inputValueSum = inputOne + inputTwo + inputThree;
+
+  if (
+    stickerInputOne.value >= 1 &&
+    stickerInputTwo.value >= 1 &&
+    stickerInputThree.value >= 1
+  ) {
+    totalNumberSticker.textContent = inputValueSum;
+  }
+  if (inputValueSum > 10) {
+    stickerWarning.classList.remove("hidden");
+  } else {
+    stickerWarning.classList.add("hidden");
   }
 });
